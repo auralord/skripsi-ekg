63bedf9ebf2b23682ed869ae55ba3f50e4bfe44d
auralord <reizasine@gmail.com>
2017-05-03 11:20:29 +0700

    Added constants for server-service comm

---
+++ persistance/src/main/java/com/reach/ekg/persistence/Message.java
@@ -0,0 +1,11 @@
+package com.reach.ekg.persistence;
+
+public class Message {
+
+    public static final String SVM_PARAMS = "svmParams";
+    public static final String GA_PARAMS = "gaParams";
+    public static final String REPEAT = "repeat";
+    public static final String LABEL = "label";
+    public static final String STATE = "state";
+
+}
