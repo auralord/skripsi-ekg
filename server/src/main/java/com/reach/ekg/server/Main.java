976e97447d1523ded979bb22425e8fe2260cde62
auralord <reizasine@gmail.com>
2017-06-03 22:06:43 +0700

    Added endpoint for chart data

--- server/src/main/java/com/reach/ekg/server/Main.java
+++ server/src/main/java/com/reach/ekg/server/Main.java
@@ -2,8 +2,6 @@ package com.reach.ekg.server;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.reach.ekg.persistence.params.GAParams;
-import com.reach.ekg.persistence.params.SVMParams;
 import com.reach.ekg.server.controllers.History;
 import com.reach.ekg.server.controllers.JobManager;
 import com.reach.ekg.server.controllers.NewJob;
@@ -36,13 +34,13 @@ public class Main {
         // Endpoints for service communication
         JobManager manager = new JobManager();
         Spark.path("/service", () -> {
-            Spark.get("/status",    manager::status,    mapper::writeValueAsString);
-            Spark.post("/start",    manager::start,     mapper::writeValueAsString);
-            Spark.post("/update",   manager::update,    mapper::writeValueAsString);
-            Spark.post("/finish",   manager::finish,    mapper::writeValueAsString);
-            Spark.post("/reset",    manager::reset,     mapper::writeValueAsString);
+            Spark.get("/status", manager::status, mapper::writeValueAsString);
+            Spark.post("/start", manager::start, mapper::writeValueAsString);
+            Spark.post("/update", manager::update, mapper::writeValueAsString);
+            Spark.post("/finish", manager::finish, mapper::writeValueAsString);
+            Spark.post("/reset", manager::reset, mapper::writeValueAsString);
 
-            Spark.afterAfter("/*",  (req, res) -> {
+            Spark.afterAfter("/*", (req, res) -> {
                 if (res.status() != 404) {
                     res.type("application/json");
                 }
@@ -60,37 +58,6 @@ public class Main {
 
         Results results = new Results();
         Spark.get("results/:id/:testNum", results::viewTestDetails);
-
-        // Only for testing
-        Spark.get("/add-job/:name/:repeat", (req, res) -> {
-            try {
-                String label = req.params("name");
-                int repeat = Integer.valueOf(req.params("repeat"));
-                SVMParams svmParams = new SVMParams()
-                        .setLambda(0.5)
-                        .setGamma(0.01)
-                        .setC(1)
-                        .setEpsilon(0.00001)
-                        .setThreshold(0)
-                        .setMaxIter(100)
-                        .setKernelParam(2);
-
-                GAParams gaParams = new GAParams()
-                        .setCr(0.9)
-                        .setMr(0.1)
-                        .setGeneration(10)
-                        .setPopSize(10);
-
-                boolean success = manager.addJob(label, svmParams, gaParams, repeat);
-                if (success) {
-                    return "Job added";
-                } else {
-                    return "Can't add job";
-                }
-            } catch (NumberFormatException e) {
-                System.err.println("ERROR: " + e.getMessage());
-                return "Must be number";
-            }
-        });
+        Spark.get("results/:id/:testNum/history-csv", results::getFitnessHistoryCSV);
     }
 }
